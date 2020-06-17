86c4f11317074469bc19fc9c1b7c9bff172b31fa
Zhaoyang Hou <florianhou@hotmail.com>
2020-06-17 20:59:25 +0200

    landmarkDetection.py hinzufuegen

---
+++ Test/vision_landMarkDetection.py
@@ -0,0 +1,12 @@
+import qi
+import time
+import sys
+import argparse
+
+class LandmarkDetector(object):
+    """
+    Find the landMark and do something
+    """
+
+    def __init__(self):
+        super(LandmarkDetector, self).__init__()
